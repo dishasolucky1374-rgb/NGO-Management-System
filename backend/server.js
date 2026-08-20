d4d3a64d0de8b158eff13848b68ecc762ba788ba
dishasolucky1374-rgb <dishasolucky1374@gmail.com>
2026-08-20 13:47:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,8 @@ app.get("/", (req, res) => {
   res.send("NGO Backend is Running!");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
