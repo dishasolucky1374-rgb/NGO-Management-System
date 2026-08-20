3b84b6dafc052afb9bd4e8f3a27a2c8d44c6f482
dishasolucky1374-rgb <dishasolucky1374@gmail.com>
2026-08-20 11:48:18 +0530

    Update App.jsx

--- frontend/ngo-frontend/src/App.jsx
+++ frontend/ngo-frontend/src/App.jsx
@@ -1,1 +1,48 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import OurWork from "./pages/OurWork";
+import Projects from "./pages/Projects";
+import Media from "./pages/Media";
+import GetInvolved from "./pages/GetInvolved";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Donate from "./pages/Donate";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Account from "./pages/Account";
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/our-work" element={<OurWork />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/media" element={<Media />} />
+        <Route path="/get-involved" element={<GetInvolved />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/account" element={<Account />} />
+        
+      </Routes>
+
+      <Footer />
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
