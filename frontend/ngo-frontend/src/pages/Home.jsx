27f607ba5c050a690d7c95eba919acf2792095fd
dishasolucky1374-rgb <dishasolucky1374@gmail.com>
2026-09-05 10:01:08 +0530

    Update Home.jsx

--- frontend/ngo-frontend/src/pages/Home.jsx
+++ frontend/ngo-frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import "./Home.css";
+import "./home.css";
 
 function Home() {
   const [banners, setBanners] = useState([]);
